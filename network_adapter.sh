b5fe986378c1d207f37cdb6e08fc8f559054670f
SebelSec <sebettieri@yahoo.com>
2023-02-24 12:46:53 -0300

    fix network_adapter.sh

--- network_adapter.sh
+++ network_adapter.sh
@@ -20,22 +20,24 @@ function selectIface(){  #Función que selecciona la interfaz
     )
 
     echo $listed_ifaces
-
+    echo -e "\n[0] Atras.. "
     echo -e "\n" 
     echo -e "-----------------------------------\n"
-    echo "[!] Seleccione interfaz por número"
+    echo -e "[!] Seleccione interfaz por número \n"
 
     read number
 
     selectedIface="$( echo $listed_ifaces | grep -F "[$number]" | awk '{print $3}')" #Al grepear [$number], no falla la validación
 
-    echo $listed_ifaces | grep -F "[$number]"
-
-    if [ $? -eq 0 ]; then
-        echo "La interfaz seleccionada es $selectedIface"
+    if [ $number -eq 0 ]; then
+        selectedIface="No seleccionado"
         read -rs -p"Presiona una tecla para continuar";echo
         clear
-        ifacesopt 
+    elif [ ${#selectedIface} -gt 0 ]; then
+        clear
+        echo -e "\n [!] La interfaz seleccionada es [${selectedIface}] \n"
+        read -rs -p"Presiona una tecla para continuar";echo
+        clear
     else
         echo "No existe la interfaz"
         selectedIface="No seleccionado"
@@ -47,20 +49,31 @@ function selectIface(){  #Función que selecciona la interfaz
 
 function ifaceInfo(){  #Muestra información de la interfaz
 
-echo "infooo"
-    # if [ "$selectedIface" != "No seleccionado" ];
-    # then
-    #     echo -e "ifconfig $selectedIface"
-    # else
-    #     menuifaces
-    # fi
+    if [ "$selectedIface" != "No seleccionado" ];
+    then
+        ip=$(ifconfig $selectedIface | head -n 2 | tail -n 1 | awk '{print $2}')
+        netmask=$(ifconfig $selectedIface | head -n 2 | tail -n 1 | awk '{print $4}')
+
+        echo -e "----  Información de interfaz [ $selectedIface ]  -----\n"
+        echo -e "IP: $ip \n"
+        echo -e "Máscara de subred: $netmask \n"
+        
+        read -rs -p"Presiona una tecla para continuar";echo
+        clear
+        menuOpt=""
+        menuifaces
+    else
+        menuOpt=""
+        menuifaces
+    fi
 }
 
 
 
 function ifacesopt(){  #Muestra las opciones y elige el menú
-    echo "Interfaz de red actual: [ $selectedIface ]"
-    echo "----------------------------------"
+    echo -e "----------------------------------"
+    echo -e "Interfaz de red actual: [ $selectedIface ]"
+    echo -e "----------------------------------\n"
     echo -e "[ 1 ] Elegir interfaz de red \n"
 
     if [ "$selectedIface" != "No seleccionado" ]; then
@@ -68,21 +81,34 @@ function ifacesopt(){  #Muestra las opciones y elige el menú
         echo -e "[ 3 ] Ver información de la interfaz de red"
     fi
 
-    echo -e "[ 0 ] Salir \n"
-
-    read menuOpt
+    echo -e "\n[ 0 ] Salir \n"
 }
 
 
 function menuifaces(){
     clear
-    ifacesopt
     case $menuOpt in
-        1) selectIface;;
-        2) selectHosts;;
-        3) ifaceInfo;;
-        *) echo "opcion inválida";;
+        1) 
+            selectIface
+            ifacesopt
+            read menuOpt
+        ;;
+        2) selectHosts
+        
+        ;;
+        3) ifaceInfo
+        
+        ;;
+        "")
+            ifacesopt
+            read menuOpt
+        ;;
+        *)
+            ifacesopt
+            read menuOpt
+        ;;
         0) exit 0
+        ;;
     esac
 
 }
